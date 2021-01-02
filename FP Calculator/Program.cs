760ad47b7b4d3a91d6533667711de6166b86071d
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-02 13:36:46 -0800

    Added basic functions will add more later

--- FP Calculator/Program.cs
+++ FP Calculator/Program.cs
@@ -6,12 +6,33 @@ namespace FP_Calculator
     {
         static void Main(string[] args)
         {
+            /*
+             Functions
+             */
+
             int add(int x, int y)
             {
                 return x + y;
             }
 
-            Console.WriteLine(add(2, 2));
+            int sub(int x, int y)
+            {
+                return x - y;
+            }
+
+            int multiply(int x, int y)
+            {
+                return x * y;
+            }
+
+            int divide(int x, int y)
+            {
+                return x / y;
+            }
+
+
+
+            
         }
     }
 }
