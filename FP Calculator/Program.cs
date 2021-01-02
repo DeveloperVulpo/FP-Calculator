275f138dac37d7405c26eb93367c3c9e1e30d4dd
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-02 13:15:21 -0800

    Add project files.

---
+++ FP Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FP_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int add(int x, int y)
+            {
+                return x + y;
+            }
+
+            Console.WriteLine(add(2, 2));
+        }
+    }
+}
