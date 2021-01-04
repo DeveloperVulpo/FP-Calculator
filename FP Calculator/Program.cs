f75ba2abdcb08ce23a28796f5f95e5f020e9fdae
Jason Diaz Jimenez <developervulpo@outlook.com>
2021-01-03 19:34:55 -0600

    Lukas is forcing me to captize me methods

--- FP Calculator/Program.cs
+++ FP Calculator/Program.cs
@@ -2,36 +2,77 @@
 
 namespace FP_Calculator
 {
+
+
     class Program
     {
+
+        /*
+            Functions
+            */
+
+        static int add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        static int multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static int divide(int x, int y)
+        {
+            return x / y;
+        }
+
+        /*
+        Initalizer 
+        */
+
+        static void start()
+        {
+            {
+                Console.Write("What is your x value: ");
+                int x = Int32.Parse(Console.ReadLine());
+                Console.Write("What is your y value: ");
+                int y = Int32.Parse(Console.ReadLine());
+                Console.Write("What is your operation? ( +, - , *, or / ): ");
+                string operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine(add(x, y));
+                        break;
+                    case "-":
+                        Console.WriteLine(sub(x, y));
+                        break;
+                    case "*":
+                        Console.WriteLine(multiply(x, y));
+                        break;
+                    case "/":
+                        Console.WriteLine(divide(x, y));
+                        break;
+                    default:
+                        Console.WriteLine("Hey! That's not a number >:(");
+                        break;
+                }
+
+            }
+        }
+
+
+
         static void Main(string[] args)
         {
-            /*
-             Functions
-             */
-
-            int add(int x, int y)
-            {
-                return x + y;
+            while(true){
+            start();
             }
-
-            int sub(int x, int y)
-            {
-                return x - y;
-            }
-
-            int multiply(int x, int y)
-            {
-                return x * y;
-            }
-
-            int divide(int x, int y)
-            {
-                return x / y;
-            }
-
-
-
             
         }
     }
